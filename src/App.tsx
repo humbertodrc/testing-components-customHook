028da2639d248f28c4ef2ab7a5110eaa77518eab
Humberto Rivero <63797901+danieldamian09@users.noreply.github.com>
2022-11-21 19:39:33 -0300

    Add base pruebas falta mocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import useDisclosure from "./hooks/useDisclosure";
+import "./App.css";
+import { Modal } from './components/Modal';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const {
+		isOpen: modalIsOpen,
+		close: closeModal,
+		toggle: toggleModal,
+	} = useDisclosure();
+	const {
+		isOpen: sidebarIsOpen,
+		close: closeSidebar,
+		toggle: toggleSidebar,
+	} = useDisclosure();
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<div className="container">
+				<Sidebar visible={sidebarIsOpen} close={closeSidebar} />
+				<h1>useDisclosure custom hook</h1>
+				<div className="button-container">
+					<button onClick={toggleSidebar}>Open sidebar</button>
+					<button onClick={toggleModal}>Open modal</button>
+				</div>
+				<Modal visible={modalIsOpen} close={closeModal} />
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
